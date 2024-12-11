46c91e95c4d0f15433350c8dfd01b63ba8b9131b
PuffyWithEyes <klimov-danya@mail.ru>
2024-12-11 22:12:38 +0300

    Убрал хуетень

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AuthView from '../views/AboutView.vue'
+import AuthView from '../views/AuthView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
